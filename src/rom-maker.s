57856035d5c4e66041d9013d8d312d321dec63d7
Micah Cowan <micah@addictivecode.org>
2021-11-29 10:29:07 -0800

    rom-maker

---
+++ src/rom-maker.s
@@ -0,0 +1,74 @@
+.macpack apple2
+
+COUT = $fded
+CtrlD = $04
+CR   = $0D
+SavedProgStart = $35FC
+SavedProgEnd = $35FE
+PRGEND = $AF
+TEXTTAB = $67
+GETLN1 = $fd6f
+INBUF = $200
+
+        .org $4000
+
+.macro printLine_   addr
+        lda #<addr
+        sta $06
+        lda #>addr
+        sta $07
+        jsr printLine
+.endmacro
+
+Start:
+        ; BLOAD ASOFT LOADER at $3600
+        printLine_ (BLOAD_str+1)    ; once without Ctrl-D to echo to screen
+        printLine_ BLOAD_str
+
+        ; Save program start/end info
+        lda TEXTTAB
+        ldy TEXTTAB+1
+        sta SavedProgStart
+        sty SavedProgStart+1
+        lda PRGEND
+        ldy PRGEND+1
+        sta SavedProgEnd
+        sty SavedProgEnd+1
+
+        ; Prompt for rom filename
+        printLine_ PROMPT_str
+        jsr GETLN1
+        lda #00
+        sta INBUF, x        ; CR-terminated -> NUL-terminated
+
+        ; BSAVE rom file (echoing first)
+        printLine_ BSAVE_str_pre+1      ; echo
+        printLine_ INBUF
+        printLine_ BSAVE_str_post
+        printLine_ BSAVE_str_pre        ; execute
+        printLine_ INBUF
+        printLine_ BSAVE_str_post
+        rts
+
+printLine:
+        ldy #$00
+        sec
+:           lda ($06), y
+            beq :+          ; end if NUL
+            jsr COUT        ; output one char
+            iny
+        bcs :-
+:       rts
+
+BLOAD_str:
+    scrcode $04, "BLOAD ASOFT LOADER,A$3600", $0D
+    .byte $00
+PROMPT_str:
+    scrcode "ROM FILE NAME? "
+    .byte $00
+BSAVE_str_pre:
+    scrcode $04, "BSAVE "
+    .byte $00
+BSAVE_str_post:
+    scrcode ",A$800,L$3000", $0D
+    .byte $00
